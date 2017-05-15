2c48747bb44328c0c894797babe0bc59ebde6f17
drzl777 <andrew.zheng725@gmail.com>
2017-05-15 13:23:47 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Andrew Zheng"
+
+  end
+
+  get '/hometown' do
+    "My hometown is Holmdel, NJ"
+
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Sunday Candy by The Social Experiment"
+  end
+
+
 end
